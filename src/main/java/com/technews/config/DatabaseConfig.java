40151d97148824ccc8380cad85f141dc3e345586
Richard Widtmann <rwidtmann@sbcglobal.net>
2020-07-18 21:53:46 -0500

    updating datasource 9.53pm

--- src/main/java/com/technews/config/DatabaseConfig.java
+++ src/main/java/com/technews/config/DatabaseConfig.java
@@ -11,7 +11,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${mysql://bdf00fe3fecb0c:67dfa215@us-cdbr-east-02.cleardb.com/heroku_dee29790da7b248?reconnect=true}")
+    @Value("${mysql://us-cdbr-east-02.cleardb.com/heroku_dee29790da7b248?user=bdf00fe3fecb0c&password=67dfa215&reconnect=true}")
     private String dbUrl;
 
     @Bean
