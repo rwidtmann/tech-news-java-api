a454593e396cc8303fc3e0986afad594fb29c56d
Richard Widtmann <rwidtmann@sbcglobal.net>
2020-07-18 23:30:41 -0500

    update 2

--- src/main/java/com/technews/config/DatabaseConfig.java
+++ src/main/java/com/technews/config/DatabaseConfig.java
@@ -11,13 +11,13 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${jdbc:mysql://us-cdbr-east-02.cleardb.com/heroku_dee29790da7b248?user=bdf00fe3fecb0c&password=67dfa215&reconnect=true}")
-    private String dbUrl;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
+//    @Value("${jdbc:mysql://us-cdbr-east-02.cleardb.com/heroku_dee29790da7b248?user=bdf00fe3fecb0c&password=67dfa215&reconnect=true}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
 }
